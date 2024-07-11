616ce302c504da64cbec6c3833cb77f6d1704802
Arthur Anthony <arthuranthony2000@gmail.com>
2024-07-11 20:39:09 -0300

    adjust project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Component } from 'react';
+import { PostCard } from './components/PostCard';
 
 class App extends Component {
 
@@ -27,7 +28,7 @@ class App extends Component {
     })
 
     this.setState({ posts: postsAndPhotos });
-  }
+  } 
 
   render() {
     const { posts } = this.state;
@@ -36,13 +37,7 @@ class App extends Component {
       <section className="container">
         <div className="posts">
           {posts.map(post => (
-            <div className="post">
-              <img src={post.cover} alt={post.title}/>
-              <div key={post.id} className="post-content">
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-              </div>
-            </div>
+            <PostCard key={post.id} post={post}/>
           ))}
         </div>
       </section>
